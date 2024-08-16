0a31b34c0440ea63f2bffad613907721e3fef8aa
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-15 21:55:10 -0400

    fix: update dependencies and fix dependency issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById('root') as HTMLElement
+
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
